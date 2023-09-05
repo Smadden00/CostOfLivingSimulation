3f8848430d4a0af9587e888eac80b3d8fb190e6e
Smadden00 <Smadden1234567890@gmail.com>
2023-09-05 14:57:59 -0500

    updated opening message
    'living with a scarcity mindset'

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       when you barely have enough to scrape by. 
       Living like this each day consumes energy and can make planning ahead and problem solving much harder than
        it may be for someone that is living with abundance. 
-      It is important to learn what a scarcity mindset is like so that we can empathize with what our clients experience every day and deliver them a better product.</h2>
+      It is important to learn what living with a scarcity mindset is like so that we can empathize with what our clients experience every day and deliver them a better product.</h2>
       <Link href='/simulation1'><h2 className='openingButton'>Begin the simulation</h2></Link>
     </main>
   )
